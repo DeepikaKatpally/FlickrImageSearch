d40856c6e62d4bf2b7103844f06ba934ace9587a
DeepikaKatpally <deepikakatpally@gmail.com>
2024-11-21 22:12:37 -0600

    Initial Commit

---
+++ FlickrImageSearch/FlickrImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImageSearchApp.swift
+//  FlickrImageSearch
+//
+//  Created by Deepika Katpally on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
